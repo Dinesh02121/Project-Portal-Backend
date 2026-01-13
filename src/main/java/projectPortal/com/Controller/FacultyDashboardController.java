a5990339210443d749ca14cc1c56ca2c48c98564
Dinesh02121 <153421362+Dinesh02121@users.noreply.github.com>
2026-01-13 23:01:01 +0530

    Update FacultyDashboardController.java

--- src/main/java/projectPortal/com/Controller/FacultyDashboardController.java
+++ src/main/java/projectPortal/com/Controller/FacultyDashboardController.java
@@ -3,6 +3,7 @@ package projectPortal.com.Controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.io.Resource;
 import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
